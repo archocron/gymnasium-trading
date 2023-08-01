8375ffb7b3dd22491e3308a4cdbe8d25d62f710e
Alejandro Rioja Chocron <archocron@gmail.com>
2023-08-01 23:40:26 +0200

    remove dash name convention warnings

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name="gymnasium-trading",
+    name="gymnasium_trading",
     version="0.0.1",
     install_requires=["gymnasium==0.26.0", "pygame==2.1.3"],
 )
